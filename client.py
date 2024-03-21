011d05db9c9ef527c7feac3194202067ca3f5246
1Seven3Seven <pburgess2003@gmail.com>
2024-03-21 22:19:56 +1030

    A non-blocking server and client. Updates are handled separately from the loop. Bug noticed, but cannot be bothered to fix.

--- client.py
+++ client.py
@@ -1,11 +1,65 @@
 import socket
+import threading
 from ipaddress import IPv4Address
 
 import pygame
+import select
 
 from get_my_ip import get_my_ip
 
 ENCODING = "UTF-8"
+
+
+class EnemyInformation:
+    def __init__(self, my_socket: socket.socket, initial_x: int, initial_y: int):
+        self.enemy_x: int = initial_x
+        self.enemy_y: int = initial_y
+
+        self.target_x: int = initial_x
+        self.target_y: int = initial_y
+
+        self.socket: socket.socket = my_socket
+
+        self._receive_enemy_info_stop_event: threading.Event = threading.Event()
+        self._receive_enemy_info_thread: threading.Thread | None = None
+
+    @property
+    def enemy_position(self) -> tuple[int, int]:
+        return self.enemy_x, self.enemy_y
+
+    @property
+    def target_position(self) -> tuple[int, int]:
+        return self.target_x, self.target_y
+
+    def _receive_enemy_info(self) -> None:
+        while not self._receive_enemy_info_stop_event.is_set():
+            readable, _, _ = select.select([self.socket], [], [], 0.1)
+
+            if readable:
+                enemy_target_str = self.socket.recv(1024).decode(ENCODING)
+                enemy_str, target_str, *_ = enemy_target_str.split(":")
+
+                enemy_x, enemy_y, *_ = enemy_str.split(",")
+                self.enemy_x = int(enemy_x)
+                self.enemy_y = int(enemy_y)
+
+                target_x, target_y, *_ = target_str.split(",")
+                self.target_x = int(target_x)
+                self.target_y = int(target_y)
+
+    def start_receiving_enemy_info(self) -> None:
+        if self._receive_enemy_info_thread is not None:
+            return
+
+        self._receive_enemy_info_thread = threading.Thread(
+            target=self._receive_enemy_info,
+            name=f"EnemyPosition.receive_enemy_info_thread"
+        )
+        self._receive_enemy_info_thread.start()
+
+    def stop_receiving_enemy_info(self) -> None:
+        self._receive_enemy_info_stop_event.set()
+        self._receive_enemy_info_thread.join()
 
 
 def client(server_ip: IPv4Address, port: int = 8888):
@@ -31,21 +85,28 @@ def client(server_ip: IPv4Address, port: int = 8888):
     clock = pygame.time.Clock()
 
     player_position = [window_size[0] // 2, window_size[1] // 2]
-    enemy_position = player_position[:]
-    target_position = player_position[:]
+    enemy_information = EnemyInformation(my_socket, player_position[0], player_position[1])
+    enemy_information.start_receiving_enemy_info()
 
     print("Sending initial position")
     my_socket.sendall(f"Initial Position:{player_position[0]},{player_position[1]}".encode(ENCODING))
+
+    to_break: bool = False
 
     # Main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit(0)
+                to_break = True
+                break
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    exit(0)
+                    to_break = True
+                    break
+
+        if to_break:
+            break
 
         screen.fill((0, 0, 0))
 
@@ -63,25 +124,21 @@ def client(server_ip: IPv4Address, port: int = 8888):
             player_position[0] += 2
 
         pygame.draw.circle(screen, (255, 255, 255), player_position, 20)
-        pygame.draw.circle(screen, (0, 255, 0), enemy_position, 10)
-        pygame.draw.circle(screen, (255, 0, 0), target_position, 5)
-        pygame.draw.line(screen, (255, 255, 255), enemy_position, target_position, 2)
+        pygame.draw.circle(screen, (0, 255, 0), enemy_information.enemy_position, 10)
+        pygame.draw.circle(screen, (255, 0, 0), enemy_information.target_position, 5)
+        pygame.draw.line(screen, (255, 255, 255),
+                         enemy_information.enemy_position, enemy_information.target_position,
+                         2)
 
         my_socket.sendall(f"{player_position[0]},{player_position[1]}".encode(ENCODING))
-
-        msg = my_socket.recv(2048)
-        msg = msg.decode(ENCODING)
-
-        enemy_position_str, target_position_str = msg.split(":")
-        x, y = enemy_position_str.split(",")
-        enemy_position = [int(x), int(y)]
-        x, y = target_position_str.split(",")
-        target_position = [int(x), int(y)]
 
         """ABOVE"""
 
         pygame.display.flip()
         clock.tick(60)
+
+    enemy_information.stop_receiving_enemy_info()
+    my_socket.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,9 +1,55 @@
 import socket
+import threading
 from ipaddress import IPv4Address
+
+import pygame.time
+import select
 
 from get_my_ip import get_my_ip
 
 ENCODING = "UTF-8"
+
+
+class ClientPosition:
+    def __init__(self, client_socket: socket.socket, client_address: tuple[str, int], initial_x: int, initial_y: int):
+        self.x: int = initial_x
+        self.y: int = initial_y
+
+        self.client_socket: socket.socket | None = client_socket
+        self.client_address: tuple[str, int] = client_address
+
+        self._receive_position_stop_event: threading.Event = threading.Event()
+        self._receive_position_thread: threading.Thread | None = None
+
+    @property
+    def position(self):
+        return self.x, self.y
+
+    def _receive_position(self) -> None:
+        while not self._receive_position_stop_event.is_set():
+            readable, _, _ = select.select([self.client_socket], [], [], 0.1)
+
+            if readable:
+                client_position_str = self.client_socket.recv(1024).decode(ENCODING)
+
+                x, y = client_position_str.split(",")
+                self.x = int(x)
+                self.y = int(y)
+
+    def start_receiving_position(self) -> None:
+        if self._receive_position_thread is not None:
+            return
+
+        self._receive_position_thread = threading.Thread(
+            target=self._receive_position,
+            name=f"ClientPosition.receive_position_thread for client at address "
+                 f"{self.client_address[0]}:{self.client_address[1]}"
+        )
+        self._receive_position_thread.start()
+
+    def stop_receiving_position(self) -> None:
+        self._receive_position_stop_event.set()
+        self._receive_position_thread.join()
 
 
 def server(my_ip: IPv4Address | None = None, port: int = 8888):
@@ -18,15 +64,15 @@ def server(my_ip: IPv4Address | None = None, port: int = 8888):
     my_socket.listen(1)
 
     print("Waiting for client")
-    client_socket, _client_addr = my_socket.accept()
-    print(f"Client address - {_client_addr[0]}:{_client_addr[1]}")
+    client_socket, client_addr = my_socket.accept()
+    print(f"Client address - {client_addr[0]}:{client_addr[1]}")
 
     input("Press enter to start.")
 
     client_socket.sendall("Start".encode(ENCODING))
 
     print("Receiving initial position")
-    msg = client_socket.recv(2048)
+    msg = client_socket.recv(1024)
     msg = msg.decode(ENCODING)
     if not msg.startswith("Initial Position:"):
         print(f"Initial position message '{msg}' is in an incorrect format")
@@ -38,19 +84,16 @@ def server(my_ip: IPv4Address | None = None, port: int = 8888):
     print(f"Received initial position: {msg}")
 
     x, y = msg.split(",")
-    player_position = [int(x), int(y)]
 
-    enemy_position = player_position[:]
+    enemy_position = [int(x), int(y)]
+    client_position = ClientPosition(client_socket, client_addr, int(x), int(y))
+    client_position.start_receiving_position()
+
+    clock = pygame.time.Clock()
 
     while True:
-        msg = client_socket.recv(2048)
-        msg = msg.decode(ENCODING)
-
-        x, y = msg.split(",")
-        player_position = [int(x), int(y)]
-
-        x_dif = player_position[0] - enemy_position[0]
-        y_dif = player_position[1] - enemy_position[1]
+        x_dif = client_position.x - enemy_position[0]
+        y_dif = client_position.y - enemy_position[1]
 
         if abs(x_dif) > abs(y_dif):
             if x_dif < 0:
@@ -64,8 +107,10 @@ def server(my_ip: IPv4Address | None = None, port: int = 8888):
                 enemy_position[1] += 1
 
         client_socket.sendall(
-            f"{enemy_position[0]},{enemy_position[1]}:{player_position[0]},{player_position[1]}".encode(ENCODING)
+            f"{enemy_position[0]},{enemy_position[1]}:{client_position.x},{client_position.y}".encode(ENCODING)
         )
+
+        clock.tick(60)
 
 
 if __name__ == "__main__":
