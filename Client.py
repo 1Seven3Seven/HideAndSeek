6455e6d53190d17268206db7f827e304d17bccba
1Seven3Seven <pburgess2003@gmail.com>
2024-04-03 23:52:59 +1030

    And it being used in the client

--- Client.py
+++ Client.py
@@ -1,94 +1,28 @@
 import socket
-import struct
 
-from Utils import ClientMessageInfo as ClientMI
-from Utils import IndicatorByte, get_my_ip
-from Utils import ServerMessageInfo as ServerMI
+from Logger import Logger
+from ServerState import ServerState
+from Utils import get_my_ip
 
 
 def main() -> None:
-    print(f"Connecting to server")
+    Logger.log(f"Connecting to server")
     client_socket = socket.socket()
     client_socket.settimeout(2)
     try:
         client_socket.connect((str(get_my_ip()), 8888))
     except TimeoutError:
-        print("Connection request timed out")
-        print("Closing socket")
+        Logger.log("Connection request timed out")
+        Logger.log("Closing socket")
         client_socket.close()
         return
 
-    print("Connected, sending connection request")
-    to_send_bytes = ClientMI.NEW_CONNECTION_REQUEST.create_bytes()
-    print(f"\tSending bytes {to_send_bytes}")
-    try:
-        client_socket.sendall(to_send_bytes)
-    except OSError:
-        print("Error with the connection")
-        print("Closing socket")
-        client_socket.close()
-        return
+    server_state = ServerState(client_socket)
+    server_state.start_updater()
 
-    print("Waiting for indicator")
-    try:
-        indicator_byte = client_socket.recv(
-            IndicatorByte.size_in_bytes
-        )
-    except TimeoutError:
-        print("Connection request timed out")
-        print("Closing socket")
-        client_socket.close()
-        return
-
-    print(f"\tReceived bytes {indicator_byte}")
-    try:
-        indicator_int, = struct.unpack(
-            IndicatorByte.format_string,
-            indicator_byte
-        )
-    except struct.error as e:
-        print(f"When attempting to unpack received error '{e}'")
-        print("Closing socket")
-        client_socket.close()
-        return
-
-    print(f"Received indicator int {indicator_int}")
-
-    if indicator_int != ServerMI.CLIENT_SET_ID.value:
-        print("Incorrect indicator int, closing socket")
-        print("Closing socket")
-        client_socket.close()
-        return
-
-    print("Correct indicator int, reading client id")
-    try:
-        client_id_bytes = client_socket.recv(
-            ServerMI.CLIENT_SET_ID.size_in_bytes
-        )
-    except TimeoutError:
-        print("Connection request timed out")
-        print("Closing socket")
-        client_socket.close()
-        return
-
-    print(f"\tReceived bytes {client_id_bytes}")
-    try:
-        client_id, = struct.unpack(
-            ServerMI.CLIENT_SET_ID.format_string,
-            client_id_bytes
-        )
-    except struct.error as e:
-        print(f"When attempting to unpack received error {e}")
-        print("Closing socket")
-        client_socket.close()
-        return
-
-    print(f"Received client id {client_id}")
-
-    print("Closing socket")
-    client_socket.close()
-
-    print("All done")
+    Logger.log("Waiting for state updater thread to finish")
+    server_state.state_updater_thread.join()
+    server_state.stop_updater()
 
 
 if __name__ == "__main__":
