faf61db60577412d57f79c7d8af013d5fe358f48
1Seven3Seven <pburgess2003@gmail.com>
2024-04-03 23:59:27 +1030

    Removing a bunch of necessary messages for now

--- Utils/ClientMessageInfo.py
+++ Utils/ClientMessageInfo.py
@@ -5,7 +5,7 @@ class ClientMessageInfo(BaseMessageInfo):
     # Connecting or disconnecting from a server (0x0?)
     NEW_CONNECTION_REQUEST = 0x01, ""
     # RECONNECTION_REQUEST = 0x02, ""
-    DISCONNECT_NOTIFICATION = 0x03, ""
+    # DISCONNECT_NOTIFICATION = 0x03, ""
 
     # Updating the servers information (0x2?)
-    NAME_CHANGE_HEADER = 0x21, "I"
+    # NAME_CHANGE_HEADER = 0x21, "I"
