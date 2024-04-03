bf46c7c0b7b94a82083adc72fc69f3cd4859c062
1Seven3Seven <pburgess2003@gmail.com>
2024-04-03 23:35:02 +1030

    New function and removed unused function for IndicatorByte.

--- Utils/IndicatorByte.py
+++ Utils/IndicatorByte.py
@@ -1,3 +1,4 @@
+import socket
 import struct
 
 from .Constants import _ENDIANNESS
@@ -12,5 +13,17 @@ class IndicatorByte:
     size_in_bytes = struct.calcsize(format_string)
 
     @classmethod
-    def create_bytes(cls, indicator: int) -> bytes:
-        return struct.pack(cls.format_string, indicator)
+    def read_from_socket(cls, soc: socket.socket) -> int:
+        """
+        Reads the amount required for an indicator int from the socket and converts it into an int.
+
+        :param soc: The socket to read from.
+        :return: The indicator int.
+        :raises TimeoutError:
+        :raises struct.error:
+        """
+
+        indicator_bytes = soc.recv(cls.size_in_bytes)
+        indicator_int, = struct.unpack(cls.format_string, indicator_bytes)
+
+        return indicator_int
