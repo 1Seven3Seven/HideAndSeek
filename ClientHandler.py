bd88f26483b8e111ea53b393c1cdac2c4e5fac79
1Seven3Seven <pburgess2003@gmail.com>
2024-04-03 19:33:46 +1030

    Moving most of the client handling code into its own class

---
+++ ClientHandler.py
@@ -0,0 +1,100 @@
+import socket
+import struct
+import threading
+
+from Logger import Logger
+from Utils import ClientMessageInfo as ClientMI
+from Utils import IndicatorByte
+from Utils import ServerMessageInfo as ServerMI
+
+
+class ClientHandler(Logger):
+    def __init__(self, client_socket: socket.socket, client_address: tuple[str, int], client_id: int):
+        self.socket: socket.socket = client_socket
+        self.address: tuple[str, int] = client_address
+
+        self.client_id: int = client_id
+
+        self.handle_client_stop_event: threading.Event = threading.Event()
+        self.handle_client_thread: threading.Thread | None = None
+
+        self.prepend: str = f"Client {client_id: 3} :"
+
+    def log(self, *args, **kwargs):
+        super().log(self.prepend, *args, **kwargs)
+
+    def start(self) -> None:
+        """
+        Starts this client handler.
+        """
+
+        if self.handle_client_thread is not None:
+            return
+
+        self.log("Starting client handler")
+
+        self.handle_client_stop_event.clear()
+
+        self.handle_client_thread = threading.Thread(
+            target=self.__handle_client
+        )
+        self.handle_client_thread.start()
+
+    def perform_handshake(self) -> bool:
+        """
+        Performs the handshake with the client.
+        Closes the socket if at any point it fails.
+
+        :return: True if completed successfully, False otherwise.
+        """
+
+        self.log("Performing handshake")
+
+        self.log("Waiting for indicator bytes")
+        try:
+            indicator_bytes = self.socket.recv(
+                IndicatorByte.size_in_bytes
+            )
+        except TimeoutError:
+            self.log("Connection timed out")
+            return False
+
+        self.log("Converting to integer")
+        try:
+            indicator_int, = struct.unpack(
+                IndicatorByte.format_string,
+                indicator_bytes
+            )
+        except struct.error as e:
+            self.log(f"Received error '{e}'")
+            return False
+        self.log(f"Indicator int is {indicator_int}")
+
+        if indicator_int != ClientMI.NEW_CONNECTION_REQUEST.value:
+            self.log("Incorrect indicator int")
+            return False
+
+        self.log("Sending client id to client")
+        try:
+            self.socket.sendall(
+                ServerMI.CLIENT_SET_ID.create_bytes(self.client_id)
+            )
+        except OSError as e:
+            self.log(f"Received error '{e}'")
+            return False
+
+        # All done
+        return True
+
+    def __handle_client(self) -> None:
+        self.log("Handler thread started")
+
+        handshake_result = self.perform_handshake()
+
+        if not handshake_result:
+            self.log("Handshake failed, closing socket")
+            self.socket.close()
+            return
+
+        self.log("Handshake successful")
+        return
--- Server.py
+++ Server.py
@@ -1,91 +1,21 @@
 import socket
-import struct
 import threading
 
 import select
 
-from Utils import ClientMessageInfo as ClientMI
-from Utils import IndicatorByte, get_my_ip
-from Utils import ServerMessageInfo as ServerMI
+from ClientHandler import ClientHandler
+from Logger import Logger
+from Utils import get_my_ip
 
 next_client_id = 1
 
-client_id_to_socket: dict[int, socket.socket] = {}
-client_id_to_address: dict[int, tuple[str, int]] = {}
-client_id_to_handshake_thread: dict[int, threading.Thread] = {}
-
-neatness_lock: threading.Lock = threading.Lock()
-
-
-def log(*_args, **_kwargs) -> None:
-    with neatness_lock:
-        print(*_args, **_kwargs)
-
-
-def handshake(client_socket: socket.socket, client_id: int) -> None:
-    """
-    Performs the initial handshake with the client given its socket and id.
-
-    :param client_socket: The socket for the client.
-    :param client_id: The id for the client.
-    """
-
-    log(f"Client {client_id: 3} : Performing handshake")
-
-    log(f"Client {client_id: 3} : Waiting for indicator")
-    try:
-        indicator_byte = client_socket.recv(
-            IndicatorByte.size_in_bytes
-        )
-    except TimeoutError:
-        log(f"Client {client_id: 3} : Connection request timed out")
-        log(f"Client {client_id: 3} : Closing socket")
-        client_socket.close()
-        return
-
-    log(f"Client {client_id: 3} :     Received bytes {indicator_byte}")
-    try:
-        indicator_int, = struct.unpack(
-            IndicatorByte.format_string,
-            indicator_byte
-        )
-    except struct.error as e:
-        log(f"Client {client_id: 3} : When attempting to unpack received error {e}")
-        log(f"Client {client_id: 3} : Closing socket")
-        client_socket.close()
-        return
-
-    log(f"Client {client_id: 3} : Received indicator int {indicator_int}")
-
-    if indicator_int != ClientMI.NEW_CONNECTION_REQUEST.value:
-        log(f"Client {client_id: 3} : Incorrect indicator int")
-        log(f"Client {client_id: 3} : Closing socket")
-        client_socket.close()
-        return
-
-    log(f"Client {client_id: 3} : Correct indicator int")
-
-    log(f"Client {client_id: 3} : Sending client id")
-    to_send_bytes = ServerMI.CLIENT_SET_ID.create_bytes(client_id)
-    log(f"Client {client_id: 3} :     Sending bytes {to_send_bytes}")
-    try:
-        client_socket.sendall(to_send_bytes)
-    except OSError:
-        log(f"Client {client_id: 3} : Error with the connection")
-        log(f"Client {client_id: 3} : Closing socket")
-        client_socket.close()
-        return
-
-    log(f"Client {client_id: 3} : All done")
-    log(f"Client {client_id: 3} : Closing socket")
-    client_socket.close()
-    return
+client_id_to_client_handler: dict[int, ClientHandler] = {}
 
 
 def accept_new_clients(server_socket: socket.socket, stop_event: threading.Event):
     global next_client_id
 
-    log(f"Accept Thread : Started")
+    Logger.log("Accept Thread : Started")
 
     server_socket.listen()
 
@@ -97,29 +27,24 @@ def accept_new_clients(server_socket: socket.socket, stop_event: threading.Event
 
         client_socket, client_address = server_socket.accept()
 
-        log(f"Accept Thread : Accepted client from address {client_address} with client id {next_client_id}")
+        Logger.log(f"Accept Thread : Accepted client from address {client_address} with client id {next_client_id}")
 
-        client_id_to_socket[next_client_id] = client_socket
-        client_id_to_address[next_client_id] = client_address
-
-        client_id_to_handshake_thread[next_client_id] = threading.Thread(
-            target=handshake,
-            args=(client_socket, next_client_id)
-        )
-        client_id_to_handshake_thread[next_client_id].start()
+        client_handler = ClientHandler(client_socket, client_address, next_client_id)
+        client_id_to_client_handler[next_client_id] = client_handler
+        client_handler.start()
 
         next_client_id += 1
 
-    log(f"Accept Thread : Stop event set, all finished")
+    Logger.log("Accept Thread : Stop event set, all finished")
 
 
 def main() -> None:
-    log(f"Server : Creating and binding socket")
+    Logger.log("Server : Creating and binding socket")
     server_socket = socket.socket()
     server_socket.settimeout(2)
     server_socket.bind((str(get_my_ip()), 8888))
 
-    log(f"Server : Setting up accepting thread")
+    Logger.log("Server : Setting up accepting thread")
     accept_new_clients_stop_event = threading.Event()
     accept_new_clients_thread = threading.Thread(
         target=accept_new_clients,
@@ -133,19 +58,19 @@ def main() -> None:
     except KeyboardInterrupt:
         pass
 
-    log(f"Server : Setting stop event")
+    Logger.log("Server : Setting stop event")
     accept_new_clients_stop_event.set()
-    log(f"Server : Waiting for accept thread to finish")
+    Logger.log("Server : Waiting for accept thread to finish")
     accept_new_clients_thread.join()
 
-    log(f"Server : Closing server socket")
+    Logger.log("Server : Closing server socket")
     server_socket.close()
 
-    log(f"Server : Joining all handshake threads")
-    for thread in client_id_to_handshake_thread.values():
-        thread.join()
+    Logger.log("Server : Joining all handshake threads")
+    for client_handler in client_id_to_client_handler.values():
+        client_handler.handle_client_thread.join()
 
-    log(f"Server : All done")
+    Logger.log("Server : All done")
 
 
 if __name__ == "__main__":
