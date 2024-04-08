e3780236eb533d934d4432094d953641fc55cc5c
1Seven3Seven <pburgess2003@gmail.com>
2024-04-09 00:08:31 +0930

    No need for redundant names

--- ClientHandler.py
+++ ClientHandler.py
@@ -7,5 +7,5 @@ class ClientHandler(Logger):
     def __init__(self, client_id: int, client_socket: socket.socket, client_address: tuple[str, int]):
         self.client_id: int = client_id
 
-        self.client_socket: socket.socket = client_socket
-        self.client_address: tuple[str, int] = client_address
+        self.socket: socket.socket = client_socket
+        self.address: tuple[str, int] = client_address
