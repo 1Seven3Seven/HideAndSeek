52ef968d0b70c8063eb77e204360178be07f63a0
1Seven3Seven <pburgess2003@gmail.com>
2024-04-08 23:40:17 +0930

    A nice and basic client and server. The client id list in the client is updated from the server. This is not an accurate reflection of the actual connected clients due to the reactionary nature of updating the servers own list. Thus, the next idea is for the clients to send notifications of when they are to disconnect. An abrupt connection will then also be caught faster due to actually reading form the client more often.

--- Client.py
+++ Client.py
@@ -1,6 +1,131 @@
-def main() -> None:
-    pass
+import socket
+import struct
+import threading
+from ipaddress import IPv4Address
+
+import select
+
+from Utils import *
+
+client_id_list: list[int] = []
+
+
+def server_update_handler(soc: socket.socket, stop_event: threading.Event):
+    global client_id_list
+
+    _prepend = "SUH:"
+
+    Logger.log(_prepend, "Thread started")
+
+    while not stop_event.is_set():
+        readable, _, _ = select.select([soc], [], [], 0.1)
+
+        if not readable:
+            continue
+
+        Logger.log(_prepend, "Reading indicator int from socket")
+        try:
+            indicator_int = IndicatorInt.read_from_socket(soc)
+        except struct.error:
+            Logger.log(_prepend, "Error unpacking struct, closing socket")
+            soc.close()
+            break
+
+        if indicator_int != ServerMessageInfo.CONNECTED_CLIENT_IDS.value:
+            Logger.log(_prepend, f"Incorrect indicator int {indicator_int}, closing socket")
+            soc.close()
+            break
+
+        Logger.log(_prepend, "Reading the number of client ids from the socket")
+        connected_client_ids_len_bytes = soc.recv(ServerMessageInfo.CONNECTED_CLIENT_IDS.size_in_bytes)
+        try:
+            connected_client_ids_len, = struct.unpack(
+                ServerMessageInfo.CONNECTED_CLIENT_IDS.format_string,
+                connected_client_ids_len_bytes
+            )
+        except struct.error:
+            Logger.log(_prepend, "Error unpacking bytes, closing socket")
+            soc.close()
+            break
+
+        format_str = "I" * connected_client_ids_len
+
+        connected_client_ids_bytes = soc.recv(
+            struct.calcsize(format_str)
+        )
+        try:
+            connected_client_ids = struct.unpack(format_str, connected_client_ids_bytes)
+        except struct.error:
+            Logger.log(_prepend, "Error unpacking bytes, closing socket")
+            soc.close()
+            break
+
+        client_id_list = list(connected_client_ids)
+
+        Logger.log(_prepend, f"Client id list is now {client_id_list}")
+
+    Logger.log(_prepend, "Thread exiting")
+
+
+def main(ip: IPv4Address, port: int) -> None:
+    Logger.log(f"Connecting to server at address {ip}:{port}")
+    soc = socket.socket()
+    soc.settimeout(2)
+    try:
+        soc.connect((str(ip), port))
+    except TimeoutError:
+        Logger.log("Connection attempt timed out")
+        return
+    Logger.log("Connection successful")
+    soc.settimeout(None)
+
+    Logger.log("Sending new connection request")
+    soc.sendall(
+        ClientMessageInfo.NEW_CONNECTION_REQUEST.create_bytes()
+    )
+
+    Logger.log("Receiving client id")
+    try:
+        indicator_int = IndicatorInt.read_from_socket(soc)
+    except struct.error:
+        Logger.log("Error unpacking struct, closing socket")
+        soc.close()
+        return
+
+    if indicator_int != ServerMessageInfo.CLIENT_ID.value:
+        Logger.log(f"Incorrect indicator int {indicator_int}, closing socket")
+        soc.close()
+        return
+
+    client_id_bytes = soc.recv(ServerMessageInfo.CLIENT_ID.size_in_bytes)
+    client_id, = struct.unpack(
+        ServerMessageInfo.CLIENT_ID.format_string, client_id_bytes
+    )
+    Logger.log(f"Received client id {client_id}")
+
+    Logger.log("Starting server update handler thread")
+    server_update_handler_stop_event = threading.Event()
+    server_update_handler_thread = threading.Thread(
+        target=server_update_handler,
+        args=(soc, server_update_handler_stop_event)
+    )
+    server_update_handler_thread.start()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        pass
+
+    Logger.log("Closing client")
+
+    # Cleanup
+    Logger.log("Setting server update handler stop event")
+    server_update_handler_stop_event.set()
+    Logger.log("Joining server update handler thread")
+    server_update_handler_thread.join()
 
 
 if __name__ == "__main__":
-    main()
+    # main(get_my_ip(), 8888)
+    main("localhost", 8888)
--- Server.py
+++ Server.py
@@ -1,6 +1,175 @@
-def main() -> None:
-    pass
+import queue
+import socket
+import struct
+import threading
+from ipaddress import IPv4Address
+from typing import Literal
+
+import select
+
+from Utils import *
+
+Tasks = Literal["New Client Connected"]
+
+modify_lock: threading.Lock = threading.Lock()
+
+client_id_to_socket: dict[int, socket.socket] = {}
+client_id_to_address: dict[int, tuple[str, int]] = {}
+next_client_id = 1
+
+
+def client_updater(task_queue: queue.Queue[Tasks], stop_event: threading.Event) -> None:
+    _prepend = "CU:"
+
+    Logger.log(_prepend, "Thread started")
+
+    while not stop_event.is_set():
+        try:
+            task = task_queue.get(timeout=0.1)
+        except queue.Empty:
+            continue
+
+        Logger.log(_prepend, f"Received task '{task}'")
+
+        if task == "New Client Connected":
+            Logger.log(_prepend, "Grabbing current client list")
+            with modify_lock:
+                client_id_tuple: tuple[int] = tuple(client_id_to_socket.keys())
+                client_socket_tuple: tuple[socket.socket] = tuple(client_id_to_socket.values())
+
+            len_client_id_tuple = len(client_id_tuple)
+
+            Logger.log(_prepend, "Creating bytes to send")
+            header_bytes_to_send = ServerMessageInfo.CONNECTED_CLIENT_IDS.create_bytes(
+                len_client_id_tuple
+            )
+
+            body_bytes_to_send = struct.pack(
+                "I" * len_client_id_tuple,
+                *client_id_tuple
+            )
+
+            bytes_to_send = header_bytes_to_send + body_bytes_to_send
+
+            Logger.log(_prepend, "Sending bytes")
+            need_to_re_update = False
+            for client_socket, client_id in zip(client_socket_tuple, client_id_tuple):
+                try:
+                    client_socket.sendall(bytes_to_send)
+                except OSError as e:
+                    Logger.log(_prepend, f"When sending to client id {client_id}, received error '{e}'")
+                    Logger.log(_prepend, "Closing socket and removing client")
+                    client_socket.close()
+                    with modify_lock:
+                        del client_id_to_socket[client_id]
+                        del client_id_to_address[client_id]
+
+            if need_to_re_update:
+                task_queue.put("New Client Connected")
+
+        else:
+            Logger.log(_prepend, f"Unknown task")
+
+
+def accept_new_clients(soc: socket.socket, stop_event: threading.Event, task_queue: queue.Queue[Tasks]) -> None:
+    global next_client_id
+
+    _prepend = "ANC:"
+
+    Logger.log(_prepend, "Thread started")
+
+    while not stop_event.is_set():
+        readable, _, _ = select.select([soc], [], [], 0.1)
+
+        if not readable:
+            continue
+
+        client_socket, client_address = soc.accept()
+        Logger.log(_prepend, f"Client under address {client_address[0]}:{client_address[1]} requested connection")
+
+        Logger.log(_prepend, "Reading indicator int from socket")
+        try:
+            indicator_int = IndicatorInt.read_from_socket(client_socket)
+        except struct.error:
+            Logger.log(_prepend, "Error unpacking struct, closing socket")
+            client_socket.close()
+            continue
+
+        if indicator_int != ClientMessageInfo.NEW_CONNECTION_REQUEST.value:
+            Logger.log(_prepend, f"Incorrect indicator int {indicator_int}, closing socket")
+            client_socket.close()
+            continue
+
+        Logger.log(_prepend, "Correct indicator int")
+
+        with modify_lock:
+            Logger.log(_prepend, f"Client id is {next_client_id}")
+
+            client_id_to_socket[next_client_id] = client_socket
+            client_id_to_address[next_client_id] = client_address
+
+        Logger.log(_prepend, "Sending client id to client")
+        client_socket.sendall(
+            ServerMessageInfo.CLIENT_ID.create_bytes(next_client_id)
+        )
+
+        next_client_id += 1
+
+        Logger.log(_prepend, "Adding notify task")
+        task_queue.put("New Client Connected")
+
+
+def main(ip: IPv4Address, port: int) -> None:
+    Logger.log(f"Binding socket to {ip}:{port}")
+    soc = socket.socket()
+    soc.bind((str(ip), port))
+
+    Logger.log("Listening for client connections")
+    soc.listen()
+
+    # Setting up the client updater thread
+
+    client_updater_task_queue: queue.Queue[Tasks] = queue.Queue()
+    client_updater_stop_event = threading.Event()
+
+    client_updater_thread = threading.Thread(
+        target=client_updater,
+        args=(client_updater_task_queue, client_updater_stop_event)
+    )
+    client_updater_thread.start()
+
+    # Setting up the accept new clients thread
+
+    accept_new_clients_stop_event: threading.Event = threading.Event()
+
+    accept_new_clients_thread = threading.Thread(
+        target=accept_new_clients,
+        args=(soc, accept_new_clients_stop_event, client_updater_task_queue)
+    )
+    accept_new_clients_thread.start()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        pass
+
+    Logger.log("Closing server")
+
+    # Cleanup
+    Logger.log("Setting accept new clients stop event")
+    accept_new_clients_stop_event.set()
+    Logger.log("Joining accept new clients thread")
+    accept_new_clients_thread.join()
+
+    Logger.log("Setting client updater stop event")
+    client_updater_stop_event.set()
+    Logger.log("Joining client updater thread")
+    client_updater_thread.join()
+
+    Logger.log("All done")
 
 
 if __name__ == "__main__":
-    main()
+    # main(get_my_ip(), 8888)
+    main("localhost", 8888)
