38b44d7624a2540a6c91a5bcd1a021c69bda13a1
1Seven3Seven <pburgess2003@gmail.com>
2024-04-08 15:32:32 +0930

    A great simplification of the communication. For now.

--- Utils/ClientMessageInfo.py
+++ Utils/ClientMessageInfo.py
@@ -2,10 +2,5 @@ from .BaseMessageInfo import BaseMessageInfo
 
 
 class ClientMessageInfo(BaseMessageInfo):
-    # Connecting or disconnecting from a server (0x0?)
-    NEW_CONNECTION_REQUEST = 0x01, ""
-    # RECONNECTION_REQUEST = 0x02, ""
-    # DISCONNECT_NOTIFICATION = 0x03, ""
-
-    # Updating the servers information (0x2?)
-    # NAME_CHANGE_HEADER = 0x21, "I"
+    # Connecting or disconnecting from a server (0x1?)
+    NEW_CONNECTION_REQUEST = 0x11, ""
--- Utils/ServerMessageInfo.py
+++ Utils/ServerMessageInfo.py
@@ -2,22 +2,8 @@ from .BaseMessageInfo import BaseMessageInfo
 
 
 class ServerMessageInfo(BaseMessageInfo):
-    # Handling client connections and disconnections (0x0?)
-    CLIENT_SET_ID = 0x01, "I"
-    CLIENT_TO_DISCONNECT = 0x02, ""
-    # CLIENT_RECONNECT_ACCEPTED = 0x03, ""
-    # CLIENT_RECONNECT_REJECTED = 0x04, ""
-
-    # Transferring map and position data (0x1?)
-    # MAP_DATA_HEADER = 0x11, "II"
-    # STARTING_POSITION = 0x12, "ii"
+    # Handling client connections and disconnections (0x1?)
+    CLIENT_ID = 0x01, "I"
 
     # Updating client information (0x02?)
-    CLIENT_CONNECTED_NOTIFICATION = 0x21, "I"
-    CLIENT_DISCONNECTED_NOTIFICATION = 0x22, "I"
-    # CLIENT_NAME_ACCEPTED = 0x23, ""
-    # CLIENT_NAME_REJECTED = 0x24, ""
-    # CLIENT_NEW_NAME_HEADER = 0x25, "II"
-
-    # Misc (0xF?)
-    # START_GAME = 0xF1, ""
+    CONNECTED_CLIENT_IDS = 0x21, "I"  # The number of connected clients followed by all the client id numbers
