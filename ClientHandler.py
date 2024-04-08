4fea4e4ac35d308e8feafd2dad3811839a9753a9
1Seven3Seven <pburgess2003@gmail.com>
2024-04-09 00:04:56 +0930

    A very basic ClientHandler that just stores information.

--- ClientHandler.py
+++ ClientHandler.py
@@ -1,3 +1,11 @@
-class ClientHandler:
-    def __init__(self):
-        pass
+import socket
+
+from Utils import Logger
+
+
+class ClientHandler(Logger):
+    def __init__(self, client_id: int, client_socket: socket.socket, client_address: tuple[str, int]):
+        self.client_id: int = client_id
+
+        self.client_socket: socket.socket = client_socket
+        self.client_address: tuple[str, int] = client_address
