a3a2fd27a9e77bd48b3f792a94861e79bf89bdcf
1Seven3Seven <pburgess2003@gmail.com>
2024-04-08 15:39:46 +0930

    Moving my Logger class

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -1,4 +1,5 @@
 from .ClientMessageInfo import ClientMessageInfo
 from .IndicatorInt import IndicatorInt
+from .Logger import Logger
 from .ServerMessageInfo import ServerMessageInfo
 from .get_my_ip import get_my_ip
